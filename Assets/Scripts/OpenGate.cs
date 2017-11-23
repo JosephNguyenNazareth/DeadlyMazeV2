d8cccc07d27e9cbf6973f775e9a12d4ce39883bc
JosephNguyenNazareth <iohkgn9861@gmail.com>
2017-11-23 21:10:30 +0700

    treasure

--- Assets/Scripts/OpenGate.cs
+++ Assets/Scripts/OpenGate.cs
@@ -4,33 +4,35 @@ using UnityEngine;
 public class OpenGate : MonoBehaviour {
     public int Direction;
 	public Transform finish;
+	public Transform clue;
 	public Hotbar hotbar;
     private Vector3 offset;
     private Vector3 pos;
+	private Vector3 pos1;
     void Start()
     {
-        if (Direction == 0) offset = new Vector3(0,1,0);
-        else if (Direction == 1) offset = new Vector3(0,-1,0);
-        else if (Direction == 2) offset = new Vector3(1,0,0);
-        else if (Direction == 3) offset = new Vector3(-1,0,0);
-        else if (Direction == 4) offset = new Vector3(0,0,1);
-        else if (Direction == 5) offset = new Vector3(0,0,-1);
+        if (Direction == 0) offset = new Vector3(0,20,0);
+        else if (Direction == 1) offset = new Vector3(0,-10,0);
+        else if (Direction == 2) offset = new Vector3(10,0,0);
+        else if (Direction == 3) offset = new Vector3(-10,0,0);
+        else if (Direction == 4) offset = new Vector3(0,0,10);
+        else if (Direction == 5) offset = new Vector3(0,0,-10);
 
         pos = finish.transform.position;
+		pos1 = clue.transform.position;
     }
 
     void OnTriggerEnter (Collider other) {
         if (other.CompareTag ("key")){
-            transform.position = pos + offset;
-            pos = transform.position;
-
-			int slotsInTotal = hotbar.getSlotsInTotal();
+ 			int slotsInTotal = hotbar.getSlotsInTotal();
 			Debug.Log (slotsInTotal);
 
 			for (int i = 0; i < slotsInTotal; i++)
 			{
-				if (transform.GetChild (1).GetChild (i).childCount != 0 && transform.GetChild (1).GetChild (i).GetChild (0).GetComponent<ItemOnObject> ().item.itemID == 18) {
-					Debug.Log ("UNLOCK");
+				if (hotbar.transform.GetChild (1).GetChild (i).childCount != 0 && hotbar.transform.GetChild (1).GetChild (i).GetChild (0).GetComponent<ItemOnObject> ().item.itemID == 18) {
+					finish.gameObject.transform.position = pos + offset;
+					clue.gameObject.transform.position = pos1 + offset;
+
 				}
 			}
         }
